62c511e01d3e2247860fbba23ea6028627f13ece
Tapas Das <91973187+tapas-251@users.noreply.github.com>
2023-05-29 23:50:47 +0530

    Add files via upload

---
+++ Day_2_assignment.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #### 1. We can comment a line in python using # at then start of the line in case of single ine comment.
+# #### Then we can use """(triple quotes) in case of multi line comments.
+# #### Different types of comments are : Single line comment, multiline comment and docstring comment
+
+# #### 2. Variables are containers in which we can store values which can be a string, number, list, tuple or dictionary.
+# ####   For example we can create a name like TAP and store a value in it as an object. Then we can refer to this object using the  name TAP. 
+# #### TAP = "Skyfall"
+# ####  Just type TAP and execute it to get the value 'Skyfall'
+
+# #### 3. We can convert the data type in python by two ways. First one will be the one where it is automatically done by Python like if we try to multply an integer with a float then the result becomes a float.
+# ####   The second one will be type casting manually wherein we use different functions like int(), float(), str() and dict() etc.
+
+# #### 4. Could not find it in Day 2 sessions so googled it for windows machine. Please check the below answer.
+# 
+# ####   Type CMD >> Verify your terminal or command prompt can run Python >> Create a Python script that is error-free >> Use python your/file/name.py to run your script from the terminal
+
+# In[1]:
+
+
+# Answer for no.5
+my_list = [1, 2, 3, 4, 5]
+test_list = my_list[1:3]
+test_list
+
+
+# In[2]:
+
+
+# Answer for no.7
+age = 25
+age
+
+
+# In[3]:
+
+
+# Answer for no.8
+price = 9.99
+print(type(price))
+# The data type for the variable price is float
+
+
+# In[4]:
+
+
+# Answer for no.9
+name = "Tapas Kumar Das"
+name
+
+
+# In[5]:
+
+
+# Answer for no.10
+string = "Hello, World!"
+substring = string[7:12]
+print(substring)
+
+
+# In[ ]:
+
+
+# Answer for no.11
+is_student = True
+# If I would have not been a student then the boolean value would be False.
+
